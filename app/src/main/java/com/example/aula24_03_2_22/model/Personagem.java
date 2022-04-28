7110fa8b97ed7deb945aacf90c0120688a4dcfa6
stephany g sodre <69463672+stephanysodre@users.noreply.github.com>
2022-04-28 19:29:59 -0300

    Update Personagem.java

--- app/src/main/java/com/example/aula24_03_2_22/model/Personagem.java
+++ app/src/main/java/com/example/aula24_03_2_22/model/Personagem.java
@@ -5,11 +5,12 @@ import androidx.annotation.NonNull;
 import java.io.Serializable;
 
 public class Personagem implements Serializable {
+    // declarar variavel
     private String nome;
     private String nascimento;
     private String altura;
     private int id = 0;
-
+//variaveis publicas q apareceram
     public Personagem(String nome, String nascimento, String altura){
         this.nome = nome;
         this.nascimento = nascimento;
@@ -18,13 +19,14 @@ public class Personagem implements Serializable {
     public Personagem(){
 
     }
+    //setar nomes, nascimento, altura 
 
     public void setNome(String nome) {this.nome = nome;}
 
     public void setNascimento(String nascimento) {this.nascimento = nascimento;}
 
     public  void setAltura(String altura) {this.altura = altura;}
-
+// pegar e retornar
     public String getNome () {return nome;}
 
     public String getNascimento() {return nascimento;}
@@ -32,7 +34,7 @@ public class Personagem implements Serializable {
     public String getAltura() {return altura;}
 
     @NonNull
-    @Override
+    @Override //informa o q precisa sobrescrever
     public String toString() {return nome;}
 
     public void setId(int id) {this.id = id;}
